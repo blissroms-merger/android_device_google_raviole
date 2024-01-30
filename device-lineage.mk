b4fa6df3b6de0c030ea65a11e8fe9852413cfc36
pimpmaneaton <shaunsibu@gmail.com>
2024-01-30 02:32:09 -0700

    Build Repainter Service app
    Change-Id: I99821954612d221ca2ca4e5c0be52636ecd92d51

--- device-lineage.mk
+++ device-lineage.mk
@@ -11,6 +11,10 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 PRODUCT_PACKAGES += \
     ANGLE
 
+# Repainter integration
+PRODUCT_PACKAGES += \
+    RepainterServicePriv
+
 # EUICC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
